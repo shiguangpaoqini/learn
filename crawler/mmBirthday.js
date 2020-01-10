3406bc0afae9ab7c15061e64d6799e72e081f50c
dcry <2507648942@qq.com>
2020-01-10 20:57:07 +0800

    爬虫添加进度条

--- crawler/mmBirthday.js
+++ crawler/mmBirthday.js
@@ -9,6 +9,8 @@ const fs = require ('fs');
 const path = require('path');
 const xlsx = require ('node-xlsx');
 const puppeteer = require ('puppeteer');
+const chalk = require('chalk');
+const ProgressBar = require('progress');
 
 const crawler = async query => {
   const browser = await puppeteer.launch ({headless: true});
@@ -20,14 +22,17 @@ const crawler = async query => {
     '.repeat_list .list_ul .list_li .list_box_in .list_li';
 
   page.on ('console', msg => {
-    let log = 'page:';
-    for (let i = 0; i < msg.args ().length; ++i)
-      log += msg.args ()[i].toString ().replace ('JSHandle:', '');
-    console.log (log);
+    for (let i = 0; i < msg.args ().length; ++i){
+      let log = ''
+      if(!!msg.args ()[i].toString ().trim()){
+        log += msg.args ()[i].toString ().trim().replace ('JSHandle:', '');
+        console.log (chalk.yellow('borwser console: '+log));
+      }
+    }
   });
 
   await page.goto (
-    `https://weibo.com/1713926427/IlPPEhLH3?type=comment#_rnd1577086461542`
+    `https://weibo.com/1713926427/IoICKpBKg?type=comment#_rnd1578636214385`
   );
   await page.waitForSelector (oneLevelCommentsCls);
 
@@ -39,42 +44,52 @@ const crawler = async query => {
   await page.waitForSelector ('.layer_login_register_v2 .form_login_register');
 
   await page.waitFor (3000);
-  await page.type ('.item.username.input_wrap .W_input', '13353835931', {
+  await page.type ('.item.username.input_wrap .W_input', query.user.username, {
     delay: 100,
   });
-  await page.type ('.item.password.input_wrap .W_input', 'song410224', {
+  await page.type ('.item.password.input_wrap .W_input', query.user.password, {
     delay: 100,
   });
   await page.click ('.form_login_register .item_btn a');
-  console.log ('login');
+  console.log (chalk.cyan('login'));
   await page.waitForNavigation();
   await page.waitForSelector (oneLevelCommentsCls + ' .list_box_in');
+  console.log (chalk.cyan('already login'));
+  await page.evaluate(() => {
+    console.log('scroll to get all hot comments')
+    window.scrollBy(0, 2000);
+  })
   await page.waitFor(3000)
-  console.log ('already login');
   await page.$$eval (oneLevelCommentsCls, (nodes, query) => {
     nodes.map (node => {
-      if (!!node.querySelector ('.WB_text').textContent.includes (`微博搞笑排行榜：${query.month}`)) {
+      if (!!node.querySelector ('.WB_text').textContent.includes (query.oneLevelCommentText)) {
         node
           .querySelector ('.list_box_in .WB_text')
           .querySelectorAll ('a')
           .forEach (ele => {
-            console.log(ele.text)
             if (ele.text.includes ('回复')) {
-              ele.click ('middle', 2);
+              ele.click ('middle');
             }
           });
       }
     });
   }, query);
+  console.log (chalk.cyan('展开二级回复'));
   await page.waitForSelector (twoLevelCommentsCls + ' .list_con');
   await page.screenshot ({path: 'test.png', fullPage: true});
-  console.log ('展开回复');
+  const bar = new ProgressBar('  展开进度 [:bar]  :current / 500 （限制最多展开500次）', {
+    complete: '>',
+    incomplete: '=',
+    width: 100,
+    total: 500
+  });
   await (async function loadMore() {
-    for (let i=0; i<400; i++) {
+    for (let i=0; i<500; i++) {
+      bar.update()
       let flag = await page.$$eval (oneLevelCommentsCls, (nodes, query) => {
         let _flag = true
         nodes.map (node => {
-          if (node.querySelector ('.WB_text').textContent.includes (`微博搞笑排行榜：${query.month}`)) {
+          if (node.querySelector ('.WB_text').textContent.includes (query.oneLevelCommentText)) {
             if(!!node.querySelector ('.list_li_v2 .WB_text')){
               node.querySelector ('.list_li_v2 .WB_text a').click ();
             } else {
@@ -84,7 +99,7 @@ const crawler = async query => {
         });
         return _flag
       }, query);
-      console.log(`展开第${i}次`, flag)
+      // console.log(`展开第${i}次`, flag)
       if(!flag) break ;
       await page.waitFor (3500);
     }
@@ -117,18 +132,22 @@ const crawler = async query => {
 
 async function main () {
   const query = {
-    month: '11月',
+    user: {
+      username: '*',
+      password: '*',
+    },
+    oneLevelCommentText: '微博搞笑排行榜：7月',
   };
   const data = await crawler (query);
   console.log(data)
   const buffer = xlsx.build ([
     {
       name: 'mmBirthday',
-      data: data,
+      data: [['昵称', '回复内容', '微博主页'], ...data],
     },
   ]);
   console.log('写入Excel文件')
-  fs.writeFileSync (path.join(__dirname, `mmBirthday-${query.month}.xlsx`), buffer, {flag: 'w'});
+  fs.writeFileSync (path.join(__dirname, `mmBirthday-${query.oneLevelCommentText}.xlsx`), buffer, {flag: 'w'});
   console.log('写入完成')
 }
 
